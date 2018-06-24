36e0ccc595b1c650f3a27f2169a76469bff50596
Joel Maximilian Mai (inf166) <joel_maximilian.mai@smail.th-koeln.de>
2018-06-24 18:04:40 +0200

    FML nichts funktioniert mehr

--- abgabe/Klausuraufgabe.c
+++ abgabe/Klausuraufgabe.c
@@ -229,7 +229,89 @@
         return (i);
     }
 
-    int main(){
+
+    //Kreiert aus den feinsten Zutaten:
+    char token_a[255]; //Tokens m�ssen au�erhalb der Funktion Deklariert werden, weil es mit "lokalem Speicher" nicht geht
+    char token_b[255];
+    char token_c[255];
+
+
+    char * strtokBobbyTippingEdition(char input[], int num) {
+
+
+        int t=0; //F�r die Whileschleifen damit die Wissen welcher Token dran ist, 0=A, 1=B, 2=C, 3=Fertig
+        int i=0; //Iterator f�r jeden char des Input Char Arrays
+        int j=0; // Iterator f�r jeden Char des jeweiligen Tokens, wird z�r�ck auf 0 gesetzt wenn Token fertig ist
+
+        printf("Input: %s\n", input); //gibt aus welchen Input die Funktion bekommen hat
+        printf("Read and Set(A): "); //Die chars f�r Token A werden festgelegt
+        while (t==0) {
+
+            token_a[j]=input[i];
+            printf("%c=%c, ", j, token_a[j], i, input[i] ); //Anzeige f�r jeweiligen Char
+            i++;
+            j++;
+            if (input[i]==32) { //32 ist der ASCII Code f�r Leerzeichen, es wird \0 gesetzt und es geht weiter zum n�chsten Token
+                token_a[j]='\0';
+                printf("\nToken A has detected Space and sets Zerobyte");
+                i++;
+                j=0;
+                t++;
+                printf("\nin Function: Token A: %s\n", token_a); //Das ist letztendlich Token A
+            }
+        }
+        printf("Read and Set(B): ");
+        while (t==1) {
+
+            token_b[j]=input[i];
+            printf("%c=%c, ", token_b[j], input[i] );
+            i++;
+            j++;
+            if (input[i]==32) {
+                token_b[j]='\0';
+                printf("\nToken B has detected Space and sets Zerobyte");
+                i++;
+                j=0;
+                t++;
+                printf("\nin Function: Token B: %s\n", token_b);
+            }
+            else if (input[i]=='\0') { //falls es nur zwei Tokens gibt wird hier bereits das \0 gesetzt
+                token_b[j]='\0';
+                printf("\nToken B has detected Zerobyte and sets Zerobyte");
+                t+=2;
+                printf("\nin Function: Token B: %s\n", token_b);
+            }
+
+        }
+        printf("Read and Set(C): ");
+        while (t==2) {
+
+            token_c[j]=input[i];
+            printf("%c=%c, ", token_c[j], input[i] );
+            i++;
+            j++;
+            if (input[i]=='\0') {
+                token_c[j]='\0';
+                printf("\nToken C has detected Zerobyte and sets Zerobyte");
+                t++;
+                printf("\nin Function: Token C: %s\n", token_c);
+            }
+
+        }
+
+        switch(num) {
+            case 0: printf("RETURN: Token A: %s\n\n", token_a); return token_a; break; //der Zweite Parameter num gibt an welcher Token returnt wird
+            case 1: printf("RETURN: Token B: %s\n\n", token_b);return token_b; break;
+            case 2: printf("RETURN: Token C: %s\n\n", token_c);return token_c; break;
+            default: return "F�r den zweiten Parameter ist nur 0-2 g�ltig";
+        }
+
+
+
+    }
+
+
+int main(){
     // Raspberry Sensoren working
     init();
     connectLCD();
@@ -288,10 +370,11 @@
             char admin_cmd[256];
             char *args2[3];
             printf("Bitte Befehl eingeben: \n");
-            scanf("%s", &admin_cmd);
-            printf("Debug: %d\n",strtoken(admin_cmd,args2,3));
-            printf("Befehl: %s IP: %s Port: %s\n", args2[0], args2[1], args2[2]);
-                    if (strcmp(args2[0], "PEERS") == 0) {     //Liste aller verbundenen Clients
+            scanf("%255[^\n]", &admin_cmd);
+//            printf("Debug: %d\n",strtoken(admin_cmd,args2,3));
+
+            printf("Befehl: %s IP: %s Port: %s\n", strtokBobbyTippingEdition(admin_cmd, 0), strtokBobbyTippingEdition(admin_cmd, 1), strtokBobbyTippingEdition(admin_cmd, 2));
+                    if (strcmp(strtokBobbyTippingEdition(admin_cmd, 0), "PEERS") == 0) {     //Liste aller verbundenen Clients
                         //TODO Tabellen ausgabe
                         int i = 0;
                         while(connectedClients[i].isNotEmpty!=0){
@@ -319,7 +402,7 @@
                             i++;
                         }
                         setLCDTextmitRGB("Alles cool yo!", LOWPRIO);
-                    } else if (strcmp(args2[0], "CONNECT") == 0) {     //Verbinde mit client via IP und PORT
+                    } else if (strcmp(strtokBobbyTippingEdition(admin_cmd, 0), "CONNECT") == 0) {     //Verbinde mit client via IP und PORT
                         //Wir machen einen Socket:
                         int client_socket; // ADRESSE ODER PROTOKOLLFAMILE / SOCKET TYP / PROTOKOLL (TCP)
                         client_socket = socket(AF_INET, SOCK_STREAM, 0);
@@ -327,12 +410,12 @@
                         //Genauere Adresse fuer den Socket
                         struct sockaddr_in serv_ad;
                         serv_ad.sin_family = AF_INET;           //Adress-Familie
-                        serv_ad.sin_port = htons((int) args2[2]);         //Portnummer
-                        printf("%d\n", (int) args2[2]);
-                        printf("%s\n", args2[1]);
-                        printf("%s\n", inet_addr(args2[1]));
+                        serv_ad.sin_port = htons((int) strtokBobbyTippingEdition(admin_cmd, 2));         //Portnummer
+                        printf("%d\n", (int) strtokBobbyTippingEdition(admin_cmd, 2));
+                        printf("%s\n", strtokBobbyTippingEdition(admin_cmd, 1));
+                        printf("%s\n", inet_addr(strtokBobbyTippingEdition(admin_cmd, 1)));
 //                        serv_ad.sin_addr.s_addr = args2[1];
-                        serv_ad.sin_addr.s_addr = inet_addr(args2[1]);
+                        serv_ad.sin_addr.s_addr = inet_addr(strtokBobbyTippingEdition(admin_cmd, 1));
 //                        serv_ad.sin_addr.s_addr = inet_ntoa(inet_addr(args2[1]));   //Vlt noch ändern in IP Adresse
                         printf("Uebernehme eingegebene IP und Port Nummern\n");
                         //     Typ              Cast zur Adresse              Länge der Adresse
@@ -348,7 +431,7 @@
                         //Ausgabe
                         printf("Dateien empfangen: %s\n", server_antwort);
                         //Verbinde mit client via IP und PORT
-                    }else if (strcmp(args2[0], "E") == 0) {
+                    }else if (strcmp(strtokBobbyTippingEdition(admin_cmd, 0), "E") == 0) {
                         close(fileDesc2);
                         //Ist das 1. Wort unbekant gibt es einne Fehlerausgabe
                     }
