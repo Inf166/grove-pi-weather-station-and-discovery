20eefd0a9fcb3a314392cdd4c69cc10ed3272db3
Bobby Tipping <Bob-tipping@web.de>
2018-07-03 14:44:12 +0200

    Sensor Initialosierungen ausgelagert

---
+++ abgabe/functions/sensors.c
@@ -0,0 +1,66 @@
+ #include <stdio.h>
+ #include "../grovepi.h"
+ #include <sys/socket.h>
+ 
+  int getColision(int pport){
+    int port = pport;
+    //if(init()==-1)
+    //    exit(1);
+    pinMode(port,INPUT);
+    if(digitalRead(port) == 0){
+        return 1;
+    }
+    }
+
+    float getTempre(int pport){
+        int port = pport;
+        float temp = 0;
+        //init();
+        pinMode(port, INPUT);
+        getTemperature(&temp, port);
+        return temp;
+    }
+
+    float getFeuchtigkeit(int pport){//Sensor an digital Port D8 anschließen
+        float humidity = 0;
+        //init();
+        int port = pport;
+        pinMode(port, INPUT);
+        getHumidity(&humidity, port);
+        return humidity;
+    }
+
+    int getGerausch(int pport){
+        int port = pport;
+        int sound = 0;
+        //init();
+        pinMode(port, INPUT);
+        sound = analogRead(port);
+        return sound;
+    }
+
+    int getBewegung(int pport){
+        int port = pport;
+        int motion = -1;
+        //if(init()==-1)
+        //    exit(1);
+        pinMode(port,INPUT);
+            motion = digitalRead(port);
+            if(motion == 255)
+                return 0;
+            if(motion == 1){ //motion detected...
+                return 1;
+            }
+    }
+
+    int getWasserkontakt(int pport){
+        int port = pport;
+        int value;
+        //if(init()==-1)
+        //    exit(1);
+        value = analogRead(port);
+        //if(value == -1)
+        //    printf("IO Error");
+        //else
+            return value;
+    }
